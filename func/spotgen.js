ec8bdacddd89df86a9a488a036b4edfcb62c0a09
Đặng Phong <thanhphongdang14@gmail.com>
2022-02-21 09:58:07 +0700

    Create spotgen.js

---
+++ func/spotgen.js
@@ -0,0 +1,36 @@
+function config(){
+    return{
+        "name": "spotgen",
+        "main": "spotgen.js",
+        "commandMap": {
+            "mong": {
+                "more": "",
+                "des": "Gen spotify account",
+                "func": "spotgen"
+            }
+        },
+        "nodeDepends":{
+            "axios" : ""
+        },
+        "author": "JustGon",
+        "version": "0.0.1"
+    }
+}
+
+async function spotgen(ctx){
+    var axios = require("axios");
+    try {
+    var {data} = await axios.get('http://justgon-dev.site/spot')
+    if(data.resultout[0].status == 'success') {
+      ctx.reply(`Success\nEmail : ${data.resultout[0].email}\nPassword : ${data.resultout[0].password}`)
+    } else {
+      ctx.reply(`Failed\nEmail : ${data.resultout[0].email}\nPassword : ${data.resultout[0].password}`)
+    } catch(err) {
+        ctx.reply(err);
+    }
+    }
+
+module.exports = {
+    spotgen,
+    config
+};
